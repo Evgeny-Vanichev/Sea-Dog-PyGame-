fd902b49b22186277090a62812a85f468f036adb
student43-1 <chegodaeva.stacy@gmail.com>
2021-12-23 17:20:25 +0300

    added function sea_travel() with some changes

--- sea_travel.py
+++ sea_travel.py
@@ -4,7 +4,6 @@ import sys
 from start_screen import start_screen
 
 FPS = 50
-tiles_group = pygame.sprite.Group()
 
 
 class Tile(pygame.sprite.Sprite):
@@ -60,57 +59,67 @@ def terminate():
     pygame.quit()
     sys.exit()
 
+def sea_travel():
+    global all_sprites
+    global player_group
+    global tile_images
+    global tiles_group
+    global tile_width
+    global tile_height
+    global player_image
 
-pygame.init()
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size)
-start_screen()
+    pygame.init()
+    size = width, height = 500, 500
+    screen = pygame.display.set_mode(size)
 
-tile_images = {
-    'rock': load_image('sea_tile.png'),
-    'empty': load_image('sea_tile.png')
-}
-player_image = load_image('ship.png')
+    tile_images = {
+        'rock': load_image('sea_tile.png'),
+        'empty': load_image('sea_tile.png')
+    }
+    player_image = load_image('ship.png')
 
-tile_width = tile_height = 50
+    tile_width = tile_height = 50
 
-player = None
+    player = None
 
-all_sprites = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+    tiles_group = pygame.sprite.Group()
 
-x, y = tile_width * 3 + 5, tile_height * 2 + 5
+    x, y = tile_width * 2, tile_height * 2
 
-player, level_x, level_y = generate_level(load_level('map.txt'))
+    player, level_x, level_y = generate_level(load_level('map.txt'))
 
-running = True
-while running:
-    for event in pygame.event.get():
-        all_sprites.draw(screen)
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x -= 50
-            if event.key == pygame.K_RIGHT:
-                x += 50
-            if event.key == pygame.K_UP:
-                y -= 50
-            if event.key == pygame.K_DOWN:
-                y += 50
+    running = True
+    while running:
+        for event in pygame.event.get():
+            all_sprites.draw(screen)
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x -= 50
+                if event.key == pygame.K_RIGHT:
+                    x += 50
+                if event.key == pygame.K_UP:
+                    y -= 50
+                if event.key == pygame.K_DOWN:
+                    y += 50
+                if x >= 500 and y >= 500:
+                    return
 
-    screen.fill((0, 0, 0))
-    tiles_group.draw(screen)
-    player_group.draw(screen)
-    font = pygame.font.Font(None, 15)
-    text = font.render('X: ' + str(x), True, (0, 0, 0))
-    text_x = 10
-    text_y = 10
-    screen.blit(text, (text_x, text_y))
-    text = font.render('Y: ' + str(y), True, (0, 0, 0))
-    text_x = 10
-    text_y = 20
-    screen.blit(text, (text_x, text_y))
-    pygame.display.flip()
+        screen.fill((0, 0, 0))
+        tiles_group.draw(screen)
+        player_group.draw(screen)
+        font = pygame.font.Font(None, 15)
+        text = font.render('X: ' + str(x), True, (0, 0, 0))
+        text_x = 10
+        text_y = 10
+        screen.blit(text, (text_x, text_y))
+        text = font.render('Y: ' + str(y), True, (0, 0, 0))
+        text_x = 10
+        text_y = 20
+        screen.blit(text, (text_x, text_y))
+        pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
