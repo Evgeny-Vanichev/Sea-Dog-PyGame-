c40b3a1d6f3c68eaea4711baf0f66ffc9eb3e770
taisia <chegodaeva.stacy@gmail.com>
2021-12-27 12:57:48 +0300

    improved functionality code view is more understandable

--- island1.py
+++ island1.py
@@ -87,75 +87,83 @@ def terminate():
     sys.exit()
 
 
-pygame.init()
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size)
-start_screen()
-sea_travel()
+def island():
+    global all_sprites
+    global house_group
+    global player_group
+    global tile_images
+    global tiles_group
+    global tile_width
+    global tile_height
+    global player_image
 
-tile_images = {
-    'road': load_image('road.png'),
-    'house': load_image('house.png'),
-    'empty': load_image('sea_tile.png')
-}
-player_image = load_image('player.png')
+    pygame.init()
+    size = width, height = 500, 500
+    screen = pygame.display.set_mode(size)
 
-tile_width = tile_height = 50
+    tile_images = {
+        'road': load_image('road.png'),
+        'house': load_image('house.png'),
+        'empty': load_image('sea_tile.png')
+    }
+    player_image = load_image('player.png')
 
-player = None
+    tile_width = tile_height = 50
 
-all_sprites = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-house_group = pygame.sprite.Group()
+    player = None
 
-x, y = tile_width * 3, tile_height * 2
+    all_sprites = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+    house_group = pygame.sprite.Group()
 
-player, level_x, level_y = generate_level(load_level('island_map.txt'))
+    x, y = tile_width * 3, tile_height * 2
 
-running = True
+    player, level_x, level_y = generate_level(load_level('island_map.txt'))
 
-camera = Camera()
+    running = True
 
-while running:
-    for event in pygame.event.get():
-        all_sprites.draw(screen)
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player.rect.x -= 50
-                if player.update() is not None:
-                    player.rect.x += 50
-            if event.key == pygame.K_RIGHT:
-                player.rect.x += 50
-                if player.update() is not None:
+    camera = Camera()
+
+    while running:
+        for event in pygame.event.get():
+            all_sprites.draw(screen)
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
                     player.rect.x -= 50
-            if event.key == pygame.K_UP:
-                player.rect.y -= 50
-                if player.update() is not None:
-                    player.rect.y += 50
-            if event.key == pygame.K_DOWN:
-                player.rect.y += 50
-                if player.update() is not None:
+                    if player.update() is not None:
+                        player.rect.x += 50
+                if event.key == pygame.K_RIGHT:
+                    player.rect.x += 50
+                    if player.update() is not None:
+                        player.rect.x -= 50
+                if event.key == pygame.K_UP:
                     player.rect.y -= 50
+                    if player.update() is not None:
+                        player.rect.y += 50
+                if event.key == pygame.K_DOWN:
+                    player.rect.y += 50
+                    if player.update() is not None:
+                        player.rect.y -= 50
 
-    camera.update(player)
-    for sprite in all_sprites:
-        camera.apply(sprite)
+        camera.update(player)
+        for sprite in all_sprites:
+            camera.apply(sprite)
 
-    screen.fill(pygame.Color("black"))
-    house_group.draw(screen)
-    tiles_group.draw(screen)
-    player_group.draw(screen)
-    font = pygame.font.Font(None, 15)
-    text = font.render('X: ' + str(x), True, (0, 0, 0))
-    text_x = 10
-    text_y = 10
-    screen.blit(text, (text_x, text_y))
-    text = font.render('Y: ' + str(y), True, (0, 0, 0))
-    text_x = 10
-    text_y = 20
-    screen.blit(text, (text_x, text_y))
-    pygame.display.flip()
+        screen.fill(pygame.Color("black"))
+        house_group.draw(screen)
+        tiles_group.draw(screen)
+        player_group.draw(screen)
+        font = pygame.font.Font(None, 15)
+        text = font.render('X: ' + str(x), True, (0, 0, 0))
+        text_x = 10
+        text_y = 10
+        screen.blit(text, (text_x, text_y))
+        text = font.render('Y: ' + str(y), True, (0, 0, 0))
+        text_x = 10
+        text_y = 20
+        screen.blit(text, (text_x, text_y))
+        pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
