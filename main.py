5e0390cc58bc71066469f1470bbaf41fd013ee6f
taisia <chegodaeva.stacy@gmail.com>
2021-12-27 13:05:39 +0300

    added main.py file starts the game

---
+++ main.py
@@ -0,0 +1,13 @@
+from start_screen import *
+from sea_travel import *
+from island1 import *
+
+
+def main():
+    start_screen()
+    sea_travel()
+    island()
+
+
+if __name__ == "__main__":
+    main()
