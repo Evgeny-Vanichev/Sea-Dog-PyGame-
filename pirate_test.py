061f0aaa42613a1ac44749656c44d2af58fb9f64
taisia <chegodaeva.stacy@gmail.com>
2021-12-27 13:00:40 +0300

    test works without mistakes need to improve radioButtons and add "game over" window

--- pirate_test.py
+++ pirate_test.py
@@ -1,33 +1,27 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QInputDialog, QLabel, QPushButton
-from PyQt5.QtGui import QPixmap, QFont
-from PyQt5.QtCore import Qt
-import sys
+from PyQt5.QtWidgets import QMainWindow
 from pirate_test_ui import Ui_MainWindow
 from questions import *
 
 
-class Test(QMainWindow, Ui_MainWindow, QApplication):
+class Test(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.count = 1
-        self.label.setText(f"Вопрос № {self.count}")
+        self.count = 0
         self.pushButton.clicked.connect(self.check)
 
     def check(self):
-        if (self.radioButton.isChecked() and questions[self.count][1][1]) \
-                or (self.radioButton_2.isChecked() and questions[self.count][2][1]) \
-                or (self.radioButton_3.isChecked() and questions[self.count][3][1]) \
-                or (self.radioButton_4.isChecked() and questions[self.count][4][1]):
-            self.sender().setEnabled(False)
-
+        if (self.radioButton.isChecked() and questions[self.count + 1][1][1]) \
+                or (self.radioButton_2.isChecked() and questions[self.count + 1][2][1]) \
+                or (self.radioButton_3.isChecked() and questions[self.count + 1][3][1]) \
+                or (self.radioButton_4.isChecked() and questions[self.count + 1][4][1]):
             self.count += 1
             if self.count == 5:
                 self.close()
-            self.label_2.setText(questions[self.count][0])
-            self.radioButton.setText(questions[self.count][1][0])
-            self.radioButton_2.setText(questions[self.count][2][0])
-            self.radioButton_3.setText(questions[self.count][3][0])
-            self.radioButton_4.setText(questions[self.count][4][0])
+            self.label_2.setText(questions[self.count + 1][0])
+            self.radioButton.setText(questions[self.count + 1][1][0])
+            self.radioButton_2.setText(questions[self.count + 1][2][0])
+            self.radioButton_3.setText(questions[self.count + 1][3][0])
+            self.radioButton_4.setText(questions[self.count + 1][4][0])
         else:
-            sys.exit()
+            pass
