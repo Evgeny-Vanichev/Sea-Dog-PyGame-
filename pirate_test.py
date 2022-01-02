61302cbaf183a68393d1bcf6707696d68242d4c0
taisia <chegodaeva.stacy@gmail.com>
2022-01-02 10:47:29 +0300

    instead of PyQT used thorpy

--- pirate_test.py
+++ pirate_test.py
@@ -1,33 +1,48 @@
-from PyQt5.QtWidgets import QMainWindow
-from pirate_test_ui import Ui_MainWindow
+import thorpy, pygame, random
 from questions import *
-from game_over import *
 from level_completed import *
+from game_over import *
 
 
-class Test(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.count = 0
-        self.pushButton.clicked.connect(self.check)
+class PirateTest(object):
+    def __init__(self, count):
+        self.count = count
+        self.label = thorpy.make_text(text=f'Вопрос №{self.count + 1}',
+                                      font_color=(0, 0, 255))
+        self.radioButton1 = thorpy.Checker(questions[self.count + 1][1][0], type_="radio")
+        self.radioButton2 = thorpy.Checker(questions[self.count + 1][2][0], type_="radio")
+        self.radioButton3 = thorpy.Checker(questions[self.count + 1][3][0], type_="radio")
+        self.radioButton4 = thorpy.Checker(questions[self.count + 1][4][0], type_="radio")
+        self.checkButton = thorpy.make_button("Проверить", func=self.check)
+        self.e_group_menu = thorpy.make_group([self.radioButton1, self.radioButton2,
+                                               self.radioButton3, self.radioButton4])
+        self.questionText = thorpy.make_text(text=questions[self.count + 1][0],
+                                             font_color=(0, 0, 255))
+        self.e_background = thorpy.Background(color=(200, 200, 255),
+                                              elements=[self.questionText,
+                                                        self.e_group_menu,
+                                                        self.checkButton])
+        thorpy.store(self.e_background, gap=20)
 
     def check(self):
-        if (self.radioButton.isChecked() and questions[self.count + 1][1][1]) \
-                or (self.radioButton_2.isChecked() and questions[self.count + 1][2][1]) \
-                or (self.radioButton_3.isChecked() and questions[self.count + 1][3][1]) \
-                or (self.radioButton_4.isChecked() and questions[self.count + 1][4][1]):
+        if (self.radioButton1.get_value() and questions[self.count + 1][1][1]) \
+                or (self.radioButton2.get_value() and questions[self.count + 1][2][1]) \
+                or (self.radioButton3.get_value() and questions[self.count + 1][3][1]) \
+                or (self.radioButton4.get_value() and questions[self.count + 1][4][1]):
             self.count += 1
-            if self.count == 5:
-                self.close()
+            if self.count >= 5:
+                thorpy.functions.quit_menu_func()
                 level_completed()
-                return
-            self.label.setText(f'Вопрос №{self.count}')
-            self.label_2.setText(questions[self.count + 1][0])
-            self.radioButton.setText(questions[self.count + 1][1][0])
-            self.radioButton_2.setText(questions[self.count + 1][2][0])
-            self.radioButton_3.setText(questions[self.count + 1][3][0])
-            self.radioButton_4.setText(questions[self.count + 1][4][0])
+            else:
+                self.e_background.update()
+                self.e_background.blit()
+                self.__init__(self.count)
+                thorpy.functions.quit_menu_func()
+                self.launch_game()
         else:
-            self.close()
+            thorpy.functions.quit_menu_func()
             game_over()
+
+    def launch_game(self):
+        menu = thorpy.Menu(self.e_background)
+        menu.play()
